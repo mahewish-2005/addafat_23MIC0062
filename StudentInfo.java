2e1034cf8fbf3ed235a470ceb42cded6c0b48e5d
mahewish-2005 <dmahewishshaik@gmail.com>
2026-04-13 17:14:09 +0530

    Update StudentInfo.java

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,7 +1,15 @@
+package Fat_23mic0062;
+
 public class StudentInfo {
-public static void main(String[] args) {
-System.out.println("Student Name: Mahewish");
-System.out.println("Register Number: 23MIC0062");
-System.out.println("Message: Jenkins–GitHub Integration Successful");
-}
+	
+		public static void main(String[] args) {
+		System.out.println("Student Name: Mahewish");
+		System.out.println("Register Number: 23MIC0062");
+		System.out.println("subject name: name:Application Development and Deployment Architecture");
+		System.out.println("course name:Computer science core");
+		System.out.println("marks:92");
+		System.out.println("grade: s");
+		System.out.println("built is successful");
+		}
+		
 }
